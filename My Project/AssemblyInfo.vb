b65c6a1dd999b16d80388c7241afa2a3e7c61325
andrew <aloesel2@gmail.com>
2022-04-14 09:49:37 -0400

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsControlLibrary1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Nexteer Automotive")>
+<Assembly: AssemblyProduct("WindowsControlLibrary1")>
+<Assembly: AssemblyCopyright("Copyright © Nexteer Automotive 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("08f22c4d-4d9d-4206-bfc0-2a4111091442")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ UserControl1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Drawing.Drawing2D
+Public Class UserControl1
+    Dim theStartColor As Color = Color.Red
+    Dim theEndColor As Color = Color.White
+    Dim theGradientMode As LinearGradientMode = LinearGradientMode.ForwardDiagonal
+
+    Property StartColor() As Color
+        Get
+            Return (theStartColor)
+        End Get
+        Set(value As Color)
+            theStartColor = value
+            Me.Invalidate()
+        End Set
+    End Property
+
+    Property EndColor() As Color
+        Get
+            Return (theEndColor)
+        End Get
+        Set(value As Color)
+            theEndColor = value
+            Me.Invalidate()
+        End Set
+    End Property
+
+    Property GradientMode() As LinearGradientMode
+        Get
+            Return (theGradientMode)
+        End Get
+        Set(value As LinearGradientMode)
+            theGradientMode = value
+            Me.Invalidate()
+        End Set
+    End Property
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        Dim myBrush As New LinearGradientBrush(Me.ClientRectangle, theStartColor, theEndColor, theGradientMode)
+        e.Graphics.FillRectangle(myBrush, Me.ClientRectangle)
+
+        myBrush.Dispose()
+
+        MyBase.OnPaint(e)
+    End Sub
+
+
+End Class
